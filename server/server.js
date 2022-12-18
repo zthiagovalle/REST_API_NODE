107a0e7d53ff5a1cb62d3ec36391dc69f4136e9f
Thiago Valle <zthiagovalle@gmail.com>
2022-12-17 22:49:43 -0300

    criando error handler para tratar as exceptions centralizadas

--- server/server.js
+++ server/server.js
@@ -3,5 +3,14 @@ const app = express();
 
 app.use(express.json());
 app.use('/', require('./route/postsRoute'));
+app.use(function (error, req, res, next) {
+    if (error.message === 'Post already exists') {
+        return res.status(409).send(error.message);
+    }
+    if (error.message === 'Post not found') {
+        return res.status(404).send(error.message);
+    }
+    res.status(500).send(error.message);
+});
 
 app.listen(3000);
--- server/service/postsService.js
+++ server/service/postsService.js
@@ -4,18 +4,25 @@ exports.getPosts = function () {
     return postsData.getPosts();
 };
 
-exports.getPost = function (id) {
-    return postsData.getPost(id);
+exports.getPost = async function (id) {
+    const post = await postsData.getPost(id);
+    if (!post) throw new Error('Post not found');
+    return post;
 };
 
-exports.savePost = function (post) {
+exports.savePost = async function (post) {
+    const existingPost = await postsData.getPostByTitle(post.title);
+    if (existingPost) throw new Error('Post already exists');
     return postsData.savePost(post);
 };
 
-exports.deletePost = function (id) {
+exports.deletePost = async function (id) {
+    const post = await postsData.getPost(id);
+    if (!post) throw new Error('Post not found');
     return postsData.deletePost(id);
 };
 
-exports.updatePost = function (id, post) {
+exports.updatePost = async function (id, post) {
+    await exports.getPost(id);
     return postsData.updatePost(id, post);
 };
